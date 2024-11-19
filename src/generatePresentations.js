c86b95ed96f56045ebca3f27c61e4893e4b14f4a
Leonardo <rmnobarra@gmail.com>
2024-11-19 18:33:56 -0300

    feat adicionando tema dracula

--- src/generatePresentations.js
+++ src/generatePresentations.js
@@ -1,15 +1,3 @@
-const fs = require('fs');
-const path = require('path');
-
-const presentationsDir = path.join(__dirname, '..', 'presentations');
-const inputDir = path.join(__dirname, '..', 'markdowns');
-
-// Criar diretório de apresentações, se não existir
-if (!fs.existsSync(presentationsDir)) {
-  fs.mkdirSync(presentationsDir);
-}
-
-// Template básico com links de CDN para Reveal.js
 const htmlTemplate = (title, content) => `
 <!doctype html>
 <html lang="en">
@@ -18,8 +6,8 @@ const htmlTemplate = (title, content) => `
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
     <title>${title}</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/reveal.js/dist/reveal.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/reveal.js/dist/theme/black.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/reveal.js/plugin/highlight/monokai.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/reveal.js/dist/theme/black.css"> <!-- Base theme -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/10.7.2/styles/dracula.min.css"> <!-- Dracula theme for code highlighting -->
   </head>
   <body>
     <div class="reveal">
@@ -39,25 +27,3 @@ const htmlTemplate = (title, content) => `
     </script>
   </body>
 </html>
-`;
-
-function convertMarkdownToHTML(filePath) {
-  const fileName = path.basename(filePath, '.md');
-  const outputFile = path.join(presentationsDir, `${fileName}.html`);
-  const content = fs.readFileSync(filePath, 'utf-8')
-    .split('\n')
-    .map(line => `<section>${line.trim()}</section>`)
-    .join('\n');
-
-  const htmlContent = htmlTemplate(fileName, content);
-
-  fs.writeFileSync(outputFile, htmlContent);
-  console.log(`Apresentação gerada: ${outputFile}`);
-}
-
-const inputFiles = fs.readdirSync(inputDir).filter(file => file.endsWith('.md'));
-
-inputFiles.forEach(file => {
-  const filePath = path.join(inputDir, file);
-  convertMarkdownToHTML(filePath);
-});
